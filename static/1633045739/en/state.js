3640cfaa82253650cd9088e928c91df66a3acdf0
Goodenl <denmatyash522@gmail.com>
2021-10-01 02:57:36 +0300

    deploy

---
+++ static/1633045739/en/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"https:\u002F\u002Fgoodenl.github.io\u002Fnakometa\u002Fstatic\u002F1633045739",layout:"default",error:null,serverRendered:true,routePath:"\u002Fen",config:{_app:{basePath:a,assetsPath:a,cdnURL:"https:\u002F\u002Fgoodenl.github.io\u002Fnakometa\u002F"}}}}("\u002F"));
